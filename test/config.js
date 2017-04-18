4c00396caeb4d2cd011105404dadc5fb7a54dea0
XWang1024 <xwang1024@126.com>
2017-04-18 14:25:07 +0800

    feature(validateSignature): 服务器签名验证

---
+++ test/config.js
@@ -0,0 +1,7 @@
+exports.testConfig = {
+  appid: 'testAppid',
+  secret: 'testSecret',
+  AESKey: 'testAES',
+  token: 'helloworld',
+  timeoutMillis: 1000
+}
--- test/wechat_api.test.js
+++ test/wechat_api.test.js
@@ -1,19 +1,58 @@
 'use strict';
 
+const _ = require('lodash');
 const assert = require('assert');
 const WechatApi = require('../lib/wechat_api');
+const testConfig = require('./config').testConfig;
 
-describe('wechat_api.test.js', function () {
-  describe('constructor(_config)', function () {
-    const customConfig = { appId: 'testId' };
-    const api = new WechatApi(customConfig);
+describe('wechat_api.test.js', () => {
+  describe('constructor', () => {
 
-    it('should set config', function () {
-      assert(api.config.appId = customConfig.appId);
+    it('should set config', () => {
+      let api = new WechatApi(testConfig);
+      assert(api.config.appid = testConfig.appid);
+      assert(api.config.secret = testConfig.secret);
+      assert(api.config.AESKey = testConfig.AESKey);
+      assert(api.config.token = testConfig.token);
+      assert(api.config.timeoutMillis = testConfig.timeoutMillis);
     });
-    
-    it('should generate new config object', function () {
-      assert(api.config !== customConfig);
+
+    it('should generate new config object', () => {
+      let api = new WechatApi(testConfig);
+      assert(api.config !== testConfig);
+    });
+
+    it('should validate config', () => {
+      let config = {}
+      try {
+        let api = new WechatApi(config);
+      } catch (e) {
+        assert(e instanceof TypeError);
+        assert(e.message.indexOf('appid') > 0);
+      }
+    });
+
+    it('should fill default props', () => {
+      let customConfig = _.omit(testConfig, 'timeoutMillis');
+      let api = new WechatApi(customConfig);
+      assert(api.config.timeoutMillis === WechatApi.defaultProps.timeoutMillis);
     });
   });
+
+  describe('validateSignature', () => {
+    it('should be a function', () => {
+      let api = new WechatApi(testConfig);
+      assert(typeof(api.validateSignature) === 'function');
+    });
+
+    it('should validate signature', () => {
+      let api = new WechatApi(testConfig);
+      let result = api.validateSignature(
+        '5cb3be3dce9725aa84012f0179d38949bba3b284',
+        '1492488976',
+        '1715648023'
+      );
+      assert(result);
+    });
+  })
 });
